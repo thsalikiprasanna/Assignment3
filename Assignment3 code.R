f751af9a1eb1f1f86f8c1dfe86c4bb470ba9c670
thsalikiprasanna <thselikiprasanna5244@gmail.com>
2020-02-02 19:46:29 -0500

    first_commit

---
+++ Assignment3 code.R
@@ -0,0 +1,11 @@
+#Name <- c("Jeb", “Donald”, "Ted”, “Marco” “Carly”, “Hillary”, “Berine”)
+
+#> ABC political poll results <- c(4, 62 51, 21, 2, 14, 15)
+
+#> CBS political poll results <- c(12, 75, 43, 19, 1, 21, 19) 
+
+political_dataframe=data.frame(name=c("Jeb", "Donald", "Ted","Marco","Carly","Hillary", "Berine"))
+
+political_dataframe['ABC_score'] = c(4, 62, 51, 21, 2, 14, 15)
+
+political_dataframe['CBS_score'] = c(12, 75, 43, 19, 1, 21, 19)
